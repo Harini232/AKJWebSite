bee0e817e6f5d3727e00baef0c72011d0dd74a2a
harini232 <harini.renganathan02@gmail.com>
2025-07-26 15:05:19 +0530

    Add project files.

---
+++ AKJWebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AKJWebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
